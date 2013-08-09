adf505f7b0beea02561b951a2a18a120b4495608
sanxzkid <f.sanxz@gmail.com>
2013-08-09 11:21:07 -0300

    Update speedometer.h

--- src/speedometer.h
+++ src/speedometer.h
@@ -11,38 +11,38 @@ class Speedometer : public QWidget
     Q_OBJECT
 
 public:
-    explicit Speedometer(QWidget *parent = 0);
-    ~Speedometer();
-
-    void setParameter(QString title, float percentage, float startScale, float endScale, QString unit);
-    void setTitle(QString title);
-    void setPercentage(float percentage);
-    void setScale(float startScale, float endEscale);
-    void setUnit(QString unit);
-    void addRange(float startRange, float endRange, QColor color);
-    void setRangeStart(int index, float startRange);
-    void setRangeEnd(int index, float endRange);
-    void setRangeColor(int index, QColor color);
-
+	explicit Speedometer(QWidget *parent = 0);
+	~Speedometer();
+	
+	void setParameter(QString title, float percentage, float startScale, float endScale, QString unit);
+	void setTitle(QString title);
+	void setPercentage(float percentage);
+	void setScale(float startScale, float endEscale);
+	void setUnit(QString unit);
+	void addRange(float startRange, float endRange, QColor color);
+	void setRangeStart(int index, float startRange);
+	void setRangeEnd(int index, float endRange);
+	void setRangeColor(int index, QColor color);
+	
 public slots:
-    void setSize(int value);
-
+	void setSize(int value);
+	
 private:
-    void paintEvent(QPaintEvent *event);
-
-    SpeedMeasure dynamic;
-    Parameter parameter;
-    QList<Range> listRange;
-
-    QPainter painter;
-    void calculateMeasure(int size);
-    void drawTitle();
-    void drawLabel();
-    void drawDisplay();
-    void drawMeter();
-    void drawRange();
-    void drawScale();
-    void drawPointer();
+	void paintEvent(QPaintEvent *event);
+	
+	SpeedMeasure dynamic;
+	Parameter parameter;
+	QList<Range> listRange;
+	
+	QPainter painter;
+	void calculateMeasure(int size);
+	void drawTitle();
+	void drawLabel();
+	void drawDisplay();
+	void drawMeter();
+	void drawRange();
+	void drawScale();
+	void drawPointer();
 };
 
 #endif
