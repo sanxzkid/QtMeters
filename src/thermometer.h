66778dba71a432f85f6fa2673e86bde7710d3ebe
sanxzkid <f.sanxz@gmail.com>
2013-08-09 11:20:20 -0300

    Update thermometer.h

--- src/thermometer.h
+++ src/thermometer.h
@@ -8,42 +8,42 @@
 
 class Thermometer : public QWidget
 {
-Q_OBJECT
+	Q_OBJECT
 
 public:
-explicit Thermometer(QWidget *parent = 0);
-~Thermometer();
-
-void setParameter(QString title, float percentage, float startScale, float endScale, QString unit);
-void setTitle(QString title);
-void setPercentage(float percentage);
-void setScale(float startScale, float endEscale);
-void setUnit(QString unit);
-void addRange(float startRange, float endRange, QColor(color));
-void setRangeStart(int index, float startRange);
-void setRangeEnd(int index, float endRange);
-void setRangeColor(int index, QColor color);
-
+	explicit Thermometer(QWidget *parent = 0);
+	~Thermometer();
+	
+	void setParameter(QString title, float percentage, float startScale, float endScale, QString unit);
+	void setTitle(QString title);
+	void setPercentage(float percentage);
+	void setScale(float startScale, float endEscale);
+	void setUnit(QString unit);
+	void addRange(float startRange, float endRange, QColor(color));
+	void setRangeStart(int index, float startRange);
+	void setRangeEnd(int index, float endRange);
+	void setRangeColor(int index, QColor color);
+	
 public slots:
-void setSize(float width, float height);
-
+	void setSize(float width, float height);
+	
 private:
-void paintEvent(QPaintEvent *event);
-
-ThermoMeasure dynamic;
-Parameter parameter;
-QList<Range> listRange;
-
-QPainter painter;	
-void calculateMeasure(float sizeWidth, float sizeHeight);
-void drawTitle();
-void drawUnit();
-void drawLabel();
-void drawDisplay();
-void drawScale();
-void drawMeter();
-void drawRange();
-void drawPointer();
+	void paintEvent(QPaintEvent *event);
+	
+	ThermoMeasure dynamic;
+	Parameter parameter;
+	QList<Range> listRange;
+	
+	QPainter painter;	
+	void calculateMeasure(float sizeWidth, float sizeHeight);
+	void drawTitle();
+	void drawUnit();
+	void drawLabel();
+	void drawDisplay();
+	void drawScale();
+	void drawMeter();
+	void drawRange();
+	void drawPointer();
 };
 
 #endif // THERMOMETER_H
